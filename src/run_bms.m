7ffbbd299580e464ef7768f774cf54707434280b
Felix Molter <moltaire1@gmail.com>
2020-11-02 20:17:29 +0100

    added code to run BMS in MATLAB, and added figure panel

---
+++ src/run_bms.m
@@ -0,0 +1,27 @@
+PROJECT_DIRECTORY = "~/Desktop/gda-context/";
+
+% Read BIC file
+bic_table = readtable((PROJECT_DIRECTORY + "results/3-behavioural-modeling/model-comparison_bics.csv"));
+
+% Read model names
+model_names = bic_table.Properties.VariableNames;
+
+% Read BIC values (drop subject index)
+bics = table2array(bic_table);
+bics = bics(:, 2:(numel(model_names)));
+
+% Convert BICs to Stephan et al format
+% From:
+% BIC = -2 * ln L + K * ln N
+% where ln L is the log likelihood of the data, K is the number of
+% parameters, and N is the number of data points
+% To:
+% BIC = ln L - (K / 2) * ln N
+bics = -2 * bics;
+
+% Run BMS
+[alpha, exp_r, xp, pxp, bor] = spm_BMS(bics);
+
+% Save result
+result = struct("model_names", model_names, "alpha", alpha, "exp_r", exp_r, "xp", xp, "pxp", pxp, "bor", bor);
+save((PROJECT_DIRECTORY + "results/3-behavioural-modeling/model-comparison_bms_results.mat"), "result");
